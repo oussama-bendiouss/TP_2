6fa95b18c0b20ddb14cb676a3b09c6a557dff548
LWAHCH <bendioussoussama@gmail.com>
2022-05-11 11:12:00 +0200

    J'ai changer la valeur du text view en utilisant la fonction runOnUiThread car une seulement la thread principale a le droit de modifier la partie graphique les layouts, le runOnUithread sera sur le try principale de Thread. J'ai utilisé une variable d pour copier le résultat s et faire une sorte de passerelle entre les différents block the try

--- app/src/main/java/com/example/tp_2/Authentification.java
+++ app/src/main/java/com/example/tp_2/Authentification.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -23,19 +24,22 @@ EditText password;
 Button Authenticate;
 String user_name;
 String pass;
+TextView Result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
-
+        Result   = findViewById(R.id.result);
         Authenticate = findViewById(R.id.connect);
+
         Thread thread = new Thread(new Runnable() {
 
             @Override
             public void run() {
                 try  {
+                    String s ="" ;
                     URL url = null;
                     try {
                         url = new URL("https://httpbin.org/basic-auth/"+user_name+"/"+pass);
@@ -45,8 +49,13 @@ String pass;
                         urlConnection.setRequestProperty ("Authorization", basicAuth);
                         try {
                             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                            String s = readStream(in);
+                            s = readStream(in);
                             Log.i("JFL", s);
+
+
+
+
+
                         } finally {
                             urlConnection.disconnect();
                         }
@@ -55,6 +64,14 @@ String pass;
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    final String d =s;
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            Result.setText("My result here is True"+d);
+                        }
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
